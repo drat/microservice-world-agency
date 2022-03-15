39982b4252e7f4c1658963433e69139c8add1179
Eric Agency <pquoctuan.no1@gmail.com>
2022-03-15 22:04:52 +0700

    .

--- api/v1_0/endpoints/facebook.py
+++ api/v1_0/endpoints/facebook.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+import base64
 from core.facebook import Facebook
 
 api_router = APIRouter()
@@ -13,5 +14,6 @@ class ReqValueChecker(BaseModel):
 
 @api_router.post('/checker')
 def checker_facebook_account(req: ReqValueChecker):
-    fb = Facebook(req.cookie)
+    cookie = base64.b64decode(req.cookie.encode('utf-8')).decode('utf-8')
+    fb = Facebook(cookie)
     return fb.check()
