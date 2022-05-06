bcec0dcff94c9f615e438ba281e85278ca446be4
Eric Agency <pquoctuan.no1@gmail.com>
2022-05-06 15:11:23 +0700

    .

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from api.v1_0.api import apiRouter
 from core.db import seed
 
 seed.initial()
-app = FastAPI(
-    title=settings.TITLE,
-    openapi_url=settings.OPENAPI_URL,
-    docs_url=settings.DOCS_URL,
-    redoc_url=None,
-    version='0.0.0'
-)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.ALLOW_ORIGINS,
-    allow_credentials=True,
-    allow_methods=settings.ALLOW_METHODS,
-    allow_headers=settings.ALLOW_HEADERS
-)
-app.include_router(apiRouter, prefix=settings.API__V1_0)
+# app = FastAPI(
+#     title=settings.TITLE,
+#     openapi_url=settings.OPENAPI_URL,
+#     docs_url=settings.DOCS_URL,
+#     redoc_url=None,
+#     version='0.0.0'
+# )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=settings.ALLOW_ORIGINS,
+#     allow_credentials=True,
+#     allow_methods=settings.ALLOW_METHODS,
+#     allow_headers=settings.ALLOW_HEADERS
+# )
+# app.include_router(apiRouter, prefix=settings.API__V1_0)
 
-if __name__ == '__main__':
-    try:
-        uvicorn.run('main:app', port=80, host='0.0.0.0', reload=True)
-    except:
-        uvicorn.run('main:app', port=8080, host='0.0.0.0', reload=True)
+# if __name__ == '__main__':
+#     try:
+#         uvicorn.run('main:app', port=80, host='0.0.0.0', reload=True)
+#     except:
+#         uvicorn.run('main:app', port=8080, host='0.0.0.0', reload=True)
