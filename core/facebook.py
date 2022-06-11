1f7cb931762663b387c089806e7b487c94f3fec4
Eric Agency <pquoctuan.no1@gmail.com>
2022-06-12 06:35:59 +0700

    .

--- core/facebook.py
+++ core/facebook.py
@@ -140,7 +140,7 @@ class Facebook:
         for cookie in cookie_list[1:]:
             requests.utils.add_dict_to_cookiejar(
                 cookie_jar, self.stringify(cookie))
-        api.cookies = cookie_jar
+        # api.cookies = cookie_jar
         api.cookies.update(cookie_jar)
 
         # api.cookies.update(self.apiParserCookieToDic(cookie))
