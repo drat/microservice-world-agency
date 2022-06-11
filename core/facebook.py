2bb86350b96a136796671902bd32192c445a6126
Eric Agency <pquoctuan.no1@gmail.com>
2022-06-12 06:24:23 +0700

    .

--- core/facebook.py
+++ core/facebook.py
@@ -135,15 +135,15 @@ class Facebook:
 
     def apiSetCookieToSession(self, api: requests.Session, cookie):
         cookie_list = json.loads(cookie)
+        cookie_jar = requests.utils.cookiejar_from_dict(
+            self.stringify(cookie_list[0]))
+        for cookie in cookie_list[1:]:
+            requests.utils.add_dict_to_cookiejar(
+                cookie_jar, self.stringify(cookie))
+        api.cookies = cookie_jar
         print(
-            self.stringify(cookie_list[0])
+            cookie_jar
         )
-        # cookie_jar = requests.utils.cookiejar_from_dict(
-        #     self.stringify(cookie_list[0]))
-        # for cookie in cookie_list[1:]:
-        #     requests.utils.add_dict_to_cookiejar(
-        #         cookie_jar, self.stringify(cookie))
-        # api.cookies = cookie_jar
 
         # api.cookies.update(self.apiParserCookieToDic(cookie))
         return api
