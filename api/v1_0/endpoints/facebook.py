c89e72059011e18c2e486f4d0b6fb6c0d0820a57
Eric Agency <pquoctuan.no1@gmail.com>
2022-06-12 16:10:56 +0700

    .

--- api/v1_0/endpoints/facebook.py
+++ api/v1_0/endpoints/facebook.py
@@ -32,7 +32,7 @@ def facebook_checker_post(req: ReqValueChecker):
         thisFacebookAccountCookie = facebook.apiParserCookieToDic(
             thisFacebookAccount.cookie
         )
-        if cookieDic['xs'] == thisFacebookAccountCookie['xs']:
+        if cookieDic['xs'] != thisFacebookAccountCookie['xs']:
             if thisFacebookAccount.graph is None:
                 raise HTTPException(status_code=403)
             else:
